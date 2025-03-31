9169b78d5b7c91a753fd3f6b72829516c9d48bb0
Andre <takememars@gmail.com>
2025-03-31 12:35:52 +0800

    [Day 4] added token expiration extension

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
+
 use Laravel\Passport\Passport;
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
 //for api authentication
 
         Passport::enablePasswordGrant();
+        Passport::tokensExpireIn(now()->addDays(30));
+        Passport::refreshTokensExpireIn(now()->addDays(60));
+        Passport::personalAccessTokensExpireIn(now()->addDays(30));
 
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
